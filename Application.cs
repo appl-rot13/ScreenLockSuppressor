09044603736e1c5fb4d4c0377a87314249e93bbc
rot <appl.rot13@gmail.com>
2021-02-12 02:25:21 +0900

    Create new

---
+++ Application.cs
@@ -0,0 +1,24 @@
+﻿
+namespace ScreenLockSuppressor
+{
+    using System;
+    using System.Threading;
+
+    using ScreenLockSuppressor.Interop;
+
+    internal static class Application
+    {
+        private static readonly TimeSpan Interval = TimeSpan.FromMinutes(1);
+
+        internal static void Main()
+        {
+            while (true)
+            {
+                User32Wrapper.MoveMouse(1, 0);
+                User32Wrapper.MoveMouse(-1, 0);
+
+                Thread.Sleep(Interval);
+            }
+        }
+    }
+}
---
+++ Interop/User32Wrapper.cs
@@ -0,0 +1,19 @@
+﻿
+namespace ScreenLockSuppressor.Interop
+{
+    using System.Runtime.InteropServices;
+
+    internal static class User32Wrapper
+    {
+        public static void MoveMouse(int dx, int dy)
+        {
+            NativeMethods.mouse_event(0x0001, dx, dy, 0, 0);
+        }
+
+        private static class NativeMethods
+        {
+            [DllImport("user32.dll")]
+            public static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+        }
+    }
+}
